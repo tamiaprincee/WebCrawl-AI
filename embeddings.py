e6f2a16b08beede96d25f60ab4596cb87fa8edf6
tamiaprincee <110130093+tamiaprincee@users.noreply.github.com>
2023-06-07 07:51:29 -0400

    Add files via upload
    Adding Files

---
+++ embeddings.py
@@ -0,0 +1,10 @@
+import tiktoken
+
+
+def remove_newlines(serie):
+    serie = serie.str.replace('\n', ' ')
+    serie = serie.str.replace('\\n', ' ')
+    serie = serie.str.replace('  ', ' ')
+    serie = serie.str.replace('  ', ' ')
+    return serie
+
